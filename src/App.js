4ed82f298aed45008f013b6500c0d175f8901859
sbelka-1703 <sbelka93@gmail.com>
2022-01-28 14:28:29 -0700

    added a .txt file and started laying out App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,22 @@ import './App.css';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="App">
+      <header className="App-header">
+
+        <p>
+
+        </p>
+        <a>
+          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m-3 rounded">
+            Get Mood
+          </button>
+          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m-3 rounded">
+            Set Mood
+          </button>
+        </a>
+      </header>
+    </div>
   );
 }
 
