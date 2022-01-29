20dbae060ac9565ece9664f10d1cf2016ef7f714
sbelka-1703 <sbelka93@gmail.com>
2022-01-28 17:39:30 -0700

    converted css to tailwind class

--- src/App.js
+++ src/App.js
@@ -3,22 +3,22 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-        <p>
-
-        </p>
-        <a>
+   
+      <div class = " bg-gray-800 min-h-screen flex flex-col justify-center items-center ">
+        
+        <input></input>
+       
+       <div>
+          
           <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m-3 rounded">
             Get Mood
           </button>
           <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m-3 rounded">
             Set Mood
           </button>
-        </a>
-      </header>
-    </div>
+        </div>
+      </div>
+    
   );
 }
 
