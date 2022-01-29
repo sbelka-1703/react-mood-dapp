851bc79ca19395a2dc2aa67e55cb99fd01c6b74c
sbelka-1703 <sbelka93@gmail.com>
2022-01-28 18:00:09 -0700

    added daisy UI and an input field

--- src/App.js
+++ src/App.js
@@ -3,22 +3,27 @@ import './App.css';
 
 function App() {
   return (
-   
+   <>
+      
+      <header className='bg-gray-800 text-white flex justify-center p-2  text-lg'>Mood Dapp</header>
       <div class = " bg-gray-800 min-h-screen flex flex-col justify-center items-center ">
-        
-        <input></input>
+      
+        <input className='input input-mg'></input>
        
        <div>
           
-          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m-3 rounded">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m-2 rounded">
             Get Mood
           </button>
-          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m-3 rounded">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m-2 rounded">
             Set Mood
           </button>
+
+          
+
         </div>
       </div>
-    
+    </>
   );
 }
 
