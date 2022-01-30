14aa73b1589efbbfab90f8fab5b9ba0d59bfbca5
sbelka-1703 <sbelka93@gmail.com>
2022-01-29 20:15:06 -0700

    Imporved styling and added poppins font

--- src/App.js
+++ src/App.js
@@ -1,30 +1,44 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-   <>
-      
-      <header className='bg-gray-800 text-white flex justify-center p-2  text-lg'>Mood Dapp</header>
-      <div class = " bg-gray-800 min-h-screen flex flex-col justify-center items-center ">
-      
-        <input className='input input-mg'></input>
-       
-       <div>
-          
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m-2 rounded">
+    <>
+
+      <header className='bg-gray-700 text-white flex justify-center items-center p-4  text-5xl'>Mood Dapp</header>
+      <div class=" bg-gray-800 min-h-screen flex flex-col justify-center items-center font-poppins">
+        <p className='text-white  p-2'>Here we can set or get the mood:</p>
+        <input className="input input-lg input-primary px-10" placeholder="Mood"></input>
+
+        <div>
+
+          <button class="btn btn-primary m-3">
             Get Mood
           </button>
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m-2 rounded">
+          <button class="btn btn-primary m-3">
             Set Mood
           </button>
-
-          
-
         </div>
+
       </div>
     </>
   );
 }
 
 export default App;
+
+
+/*  <p>
+          <input  className="input input-lg input-primary	 px-10" placeholder="Mood"></input>
+        </p>
+        <a className ="flex flex-col">
+          <div>
+          <button class="btn btn-primary m-3">
+            Get Mood
+          </button>
+          <button class="btn btn-primary m-3">
+            Set Mood
+          </button>
+          </div> 
+          
+  */
