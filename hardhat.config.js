7b580a964412a2c099f65c1277f346ebe485bb5f
sbelka-1703 <sbelka93@gmail.com>
2022-02-01 18:28:45 -0700

    configured hardhat and deployed the contract on rinkeby

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
-
+require("dotenv").config({ path: ".env" });
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -16,14 +16,16 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
+ const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
+
+ const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
  module.exports = {
   solidity: "0.8.4",
-  paths: {
-    artifacts: './src/artifacts',
-  },
   networks: {
-    hardhat: {
-      chainId: 1337
-    }
-  }
+    rinkeby: {
+      url: ALCHEMY_API_KEY_URL,
+      accounts: [RINKEBY_PRIVATE_KEY],
+    },
+  },
 };
--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 
 import './App.css';
+import { useState } from 'react';
+import { ethers } from 'ethers'
+import Mood from './artifacts/contracts/Mood.sol/MoodDiary.json'
 
 function App() {
+
+  
+  const moodAddress = "0xA4EDD8DF6cd250e2414b8B2e6d8525260087812B"
+  const[mood, setMood ] = useState()
+  
+
+
+
   return (
+    
+
     <>
 
       <header className='bg-gray-700 text-white flex justify-center items-center p-4  text-5xl'>Mood Dapp</header>
@@ -27,18 +40,3 @@ function App() {
 
 export default App;
 
-
-/*  <p>
-          <input  className="input input-lg input-primary	 px-10" placeholder="Mood"></input>
-        </p>
-        <a className ="flex flex-col">
-          <div>
-          <button class="btn btn-primary m-3">
-            Get Mood
-          </button>
-          <button class="btn btn-primary m-3">
-            Set Mood
-          </button>
-          </div> 
-          
-  */
