9c88b6daca76516505cf92b10bfc8a1328a3c1e1
sbelka-1703 <sbelka93@gmail.com>
2022-02-14 12:48:19 -0700

    Improved navbar, connect button works with web3modal!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
 
 import './App.css';
-import { useState } from 'react';
-import { ethers } from 'ethers'
+import { useEffect, useRef, useState } from "react";
+import { ethers, providers } from 'ethers'
 import Mood from './artifacts/contracts/Mood.sol/MoodDiary.json'
 import { data } from 'autoprefixer';
 import { Navbar } from './components/Navbar';
+import Web3Modal from "web3modal";
+
 
 function App() {
 
-  
+  const web3ModalRef = useRef();
+
+
   const moodAddress = "0xA4EDD8DF6cd250e2414b8B2e6d8525260087812B"
   const[mood, setMoodValue ] = useState()
   const[displayMessage, setDisplayMessage] = useState(false)
   const[moodFromSC, setMoodFromSC] = useState('Your mood is not set, please set your mood')
+  const [walletConnected, setWalletConnected] = useState(false);
+
 
   
-  async function requestAccount() {
-    await window.ethereum.request({ method: 'eth_requestAccounts' });
-  }
+  // async function requestAccount() {
+  //   await window.ethereum.request({ method: 'eth_requestAccounts' });
+  // }
+
+  const connectWallet = async () => {
+    try {
+      // Get the provider from web3Modal, which in our case is MetaMask
+      // When used for the first time, it prompts the user to connect their wallet
+      await getProviderOrSigner();
+      setWalletConnected(true);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+
+const getProviderOrSigner = async (needSigner = false) => {
+    // Connect to Metamask
+    // Since we store `web3Modal` as a reference, we need to access the `current` value to get access to the underlying object
+    const provider = await web3ModalRef.current.connect();
+    const web3Provider = new providers.Web3Provider(provider);
+
+    // If user is not connected to the Rinkeby network, let them know and throw an error
+    const { chainId } = await web3Provider.getNetwork();
+    if (chainId !== 4) {
+      window.alert("Change the network to Rinkeby");
+      throw new Error("Change network to Rinkeby");
+    }
+
+    if (needSigner) {
+      const signer = web3Provider.getSigner();
+      return signer;
+    }
+    return web3Provider;
+  };
+
+
 
   async function fetchMood() {
     if (typeof window.ethereum !== 'undefined') {
@@ -40,7 +80,7 @@ function App() {
   async function setMood() {
     if (!mood) return
     if (typeof window.ethereum !== 'undefined') {
-      await requestAccount()
+      // await requestAccount()
       const provider = new ethers.providers.Web3Provider(window.ethereum);
       const signer = provider.getSigner()
       const contract = new ethers.Contract(moodAddress, Mood.abi, signer)
@@ -63,6 +103,20 @@ function App() {
       }
   };
 
+  useEffect(() => {
+    // if wallet is not connected, create a new instance of Web3Modal and connect the MetaMask wallet
+    if (!walletConnected) {
+      // Assign the Web3Modal class to the reference object by setting it's `current` value
+      // The `current` value is persisted throughout as long as this page is open
+      web3ModalRef.current = new Web3Modal({
+        network: "rinkeby",
+        providerOptions: {},
+        disableInjectedProvider: false,
+      });
+      connectWallet();
+    }
+  }, [walletConnected]);
+
 
 
 
@@ -72,7 +126,9 @@ function App() {
 
     <>
       
-      <Navbar/>
+      <Navbar
+      connectWallet = {connectWallet}/>
+       
       <div class=" bg-gray-800 min-h-screen flex flex-col justify-center items-center font-poppins">
         
         
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 
-export const Navbar = () => {
+export const Navbar = (props) => {
     return (
 
-        <div className='from-gray-700 to-gray-600 text-white justify-between flex items-center p-4  text-xl bg-gradient-to-r '>
+        <nav className='from-gray-700 to-gray-600 text-white text-xl bg-gradient-to-r w-full h-16 box-border flex justify-between items-center px-2'>
+            <label className=''>Mood Dapp</label>
+         
 
-            <div className='flex justify-start'>
+                
+                <ul className='flex p-3 justify-between '>
+                    <li className='px-3'>Home</li>
+                    <li>About</li>
 
-                <p>Mood Dapp</p>
-            </div>
-                <div className=' flex justify-center px-10'>
                     
-                    
-                    
-                </div>
+                </ul>
+      
+                <button onClick={props.connectWallet} className='btn btn-outline btn-accent m-1'>Connect</button>
+            
 
-           
 
-        </div>)
+        </nav>)
 };
