96ed170896b968c58e563cf4166c91ad31330114
sbelka-1703 <sbelka93@gmail.com>
2022-02-16 19:05:29 -0700

    Added a styled card to the about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,10 +3,31 @@ import { Navbar } from './Navbar';
 
 export const About = (props) => {
 
-    return(
+    return (
         <>
-        <Navbar/>
-        <div>HI</div>
+            <Navbar />
+            <div className='min-h-screen bg-black flex items-center justify-center'>
+                {/* Card Goes here */}
+                <div className='relative  w-80'>
+                    {/* Yellow background */}
+                    <div className='absolute -right-5 -bottom-5 bg-yellow-400 h-full w-full rounded-xl'></div>
+
+
+                    <div className='relative bg-gray-800 text-gray-50 rounded-xl p-8 space-y-5'>
+                        {/* Yellow line */}
+                        <div className='h-1.5 w-20 bg-yellow-400'></div>
+
+                        {/*Title*/}
+                        <div className=' text-4xl font-extrabold text-white'>About</div>
+
+                        {/* Main text */}
+                        <p className=' leading-snug text-gray-400 font-poppins'>In this Mood Daap, a user can record their mood on the blockchain by entering their mood and pressing the set mood botton.  
+                        
+                        The user can also retrive their recorded mood by pressing the Get Mood button. </p>
+
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
